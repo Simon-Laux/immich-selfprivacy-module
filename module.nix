da93eff912bdb3680a30da36fad7f571b34a94c2
Simon Laux <git@simonlaux.de>
2025-09-10 15:34:38 +0200

    try allow websocket

--- module.nix
+++ module.nix
@@ -103,6 +103,9 @@ in
         add_header X-Frame-Options SAMEORIGIN;
         add_header X-Content-Type-Options nosniff;
         add_header X-XSS-Protection "1; mode=block";
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection $http_connection;
+        proxy_http_version 1.1;
       '';
       locations = {
         "/" = {
