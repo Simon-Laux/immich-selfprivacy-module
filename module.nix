97b9dae55633efbbd6f7abdd0edad41f138328c9
Simon Laux <git@simonlaux.de>
2025-09-11 00:05:26 +0200

    correct hash

--- module.nix
+++ module.nix
@@ -130,7 +130,7 @@ in
             owner = "Simon-Laux";
             repo = "immich";
             rev = "feat-allow-to-load-client-secret-from-file";
-            hash = "";
+            hash = "sha256-L30KfiP++Tqc6qUkviIkHuo/ndV7+LiNlu1F4aUyw24=";
           };
       });
     };
