c256343bb0ebf89c339356111d2a76e604e42847
Simon Laux <git@simonlaux.de>
2025-09-12 02:20:12 +0200

    start exploration to prevent first time admin logins screen

--- module.nix
+++ module.nix
@@ -149,6 +149,10 @@ in
           proxyPass = "http://localhost:2283";
           proxyWebsockets = true;
         };
+        # TODO put behind toggle
+        "/auth/register" = {
+          return = 403;
+        };
       };
     };
 
@@ -167,7 +171,7 @@ in
       enabled = true;
       autoRegister = true;
       # https://immich.app/docs/administration/oauth/#auto-launch
-      autoLaunch = false;
+      autoLaunch = true; # TODO put behind toggle
       buttonText = "Login with Kanidm";
 
       clientId = "immich";
