fa2bb4f4a4a35c387503cd3a161efd267dcea943
Simon Laux <git@simonlaux.de>
2025-09-12 02:37:18 +0200

    fix order of nginx locations

--- module.nix
+++ module.nix
@@ -144,11 +144,8 @@ in
         # FIXME is it needed?
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
       '';
+      # order matters
       locations = {
-        "/" = {
-          proxyPass = "http://localhost:2283";
-          proxyWebsockets = true;
-        };
         # TODO put behind toggle
         "/auth/register" = {
           return = 403;
@@ -156,6 +153,10 @@ in
         # TODO put behind toggle
         "/api/auth/admin-sign-up"= {
           return = 403;
+        };
+        "/" = {
+          proxyPass = "http://localhost:2283";
+          proxyWebsockets = true;
         };
       };
     };
